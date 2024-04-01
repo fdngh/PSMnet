2ae3033229afce5d37b6656a7a9fd1ebc12bf250
fdngh <152148064+fdngh@users.noreply.github.com>
2024-04-01 16:04:30 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# train
+GPUID1=0
+export CUDA_VISIBLE_DEVICES=$GPUID1
+
+####### Shared configs
+CPT="myexperiments"
+DATASET='CHAOST2_Superpix'
+NWORKER=16
+ALL_EV=(0 1 2 3 4) # 5-fold cross validation (0, 1, 2, 3, 4)
+ALL_SCALE=( "MIDDLE") # config of pseudolabels
+LABEL_SETS=1 #0 or 1
+EXCLU='[1,4]' # []for setting 1
+# LABEL_SETS=0
+# EXCLU='[2,3]'
+###### Training configs ######
+NSTEP=50100
+DECAY=0.95
+MAX_ITER=1000 # defines the size of an epoch
+SNAPSHOT_INTERVAL=10000 # interval for saving snapshot
+SEED='1234'
+###### Validation configs ######
+SUPP_ID='[4]' #  # using the additionally loaded scan as support
+echo ===================================
+for EVAL_FOLD in "${ALL_EV[@]}"
+do
+    for SUPERPIX_SCALE in "${ALL_SCALE[@]}"
+    do
+    PREFIX="test_vfold${EVAL_FOLD}"
+    echo $PREFIX
+    LOGDIR="./exps/${CPT}"
+
+    if [ ! -d $LOGDIR ]
+    then
+        mkdir $LOGDIR
+    fi
+
+    python3 te.py with \
+    'modelname=dlfcn_res101' \
+    'usealign=True' \
+    'optim_type=sgd' \
+    num_workers=$NWORKER \
+    scan_per_load=-1 \
+    label_sets=$LABEL_SETS \
+    'use_wce=True' \
+    exp_prefix=$PREFIX \
+    'clsname=grid_proto' \
+    n_steps=$NSTEP \
+    exclude_cls_list=$EXCLU \
+    eval_fold=$EVAL_FOLD \
+    dataset=$DATASET \
+    max_iters_per_load=$MAX_ITER \
+    min_fg_data=1 seed=$SEED \
+    save_snapshot_every=$SNAPSHOT_INTERVAL \
+    superpix_scale=$SUPERPIX_SCALE \
+    lr_step_gamma=$DECAY \
+    path.log_dir=$LOGDIR \
+    support_idx=$SUPP_ID
+    done
+done
